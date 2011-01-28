83a9720193647142831ece1790300b74c8de9428
rocifier@gmail.com <rocifier@gmail.com@288672fd-8508-bdd6-067b-31820365934f>
2011-01-28 20:34:23 +0000

    deleted redundant code after game screens, added commenting

--- Extinction/Extinction/Game1.cs
+++ Extinction/Extinction/Game1.cs
@@ -24,12 +24,12 @@ namespace Extinction
         {
             instance = this;
 
+            /* Create 3D device */
             graphics = new GraphicsDeviceManager(this);
 
+            /* Create game screens */
             screenManager = new ScreenManager(this);
-
             this.Components.Add(screenManager);
-
             screenManager.AddScreen(new InGameScreen());
             screenManager.AddScreen(new TitleScreen());
             
@@ -38,51 +38,21 @@ namespace Extinction
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        Model test;
-
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            test = ExtinctionGame.LoadModel(@"island_mesh",@"island_shader");
-            // TODO: use this.Content to load your game content here
-        }
-
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
+            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            UpdateMedia();
             screenManager.Update(gameTime);
-            base.Update(gameTime);
         }
-
-        Matrix view, projection;
-        Vector3 cameraPosition;
 
         protected override void Draw(GameTime gameTime)
         {
-            /*
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            cameraPosition = new Vector3(15,15,15);
-
-            view = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
-            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(70f), (float)GraphicsDevice.Viewport.Width / (float)GraphicsDevice.Viewport.Height, 1f, 100f);
-
-            // TODO: Add your drawing code here
-            test.Draw(Matrix.Identity, view, projection);
-            base.Draw(gameTime);
-             */
             screenManager.Draw(gameTime);
         }
     }
